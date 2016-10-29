46eb35713172e982fc19b3c49cbb1ef064aff2f9
21485619 <21485619@student.uwa.edu.au>
2016-10-29 19:01:36 +0800

    Initial Upload

---
+++ CITS3001/resistanceAI-master/src/test.java
@@ -0,0 +1,20 @@
+
+public class test {
+
+	
+	public static void main(String[] args){
+		int five = (int) Math.floor(4/3)+1;
+		int six = (int) Math.floor(5/3)+1;
+		int seven = (int) Math.floor(6/3)+1;
+		int eight = (int) Math.floor(7/3)+1;
+		int nine = (int) Math.floor(8/3)+1;
+		int ten = (int) Math.floor(9/3)+1;
+		
+		System.out.println("ceil(5/3) = " + five);
+		System.out.println("6/3 = " + six);
+		System.out.println("7/3 = " + seven);
+		System.out.println("8/3 = " + eight);
+		System.out.println("9/3 = " + nine);
+		System.out.println("10/3 = " + ten);
+	}
+}
